8b278dd69380ac0623aeed49860c95120941f3c6
Henrique Papile <hpapile@gmail.com>
2024-06-27 12:03:23 -0300

    ajustes

--- scripts/modules/banner.js
+++
@@ -1,8 +0,0 @@
-export default function initBannerMobile() {
-  const mobile = window.matchMedia('(max-width: 992px)').matches;
-
-  if (mobile) {
-    const banner = document.querySelector('[data-banner="banner"]');
-    banner.src = 'images/banners/02-Principal_Mobile.jpg';
-  }
-}
